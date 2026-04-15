ccb3b57752916aa89668336ed6b080d6b499c3b5
thinh <sutianer0000@gmail.com>
2026-04-15 13:27:31 +0700

    ex1lab9

---
+++ ex1lab9/BasicMath/BasicMath/BasicMaths.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMath
+{
+    public class BasicMaths
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+                throw new DivideByZeroException("Division byzero is not allowed.");
+            return num1 / num2;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+    }
+
+
+}
+
